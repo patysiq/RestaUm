572d99d6a99d25ec1b498f081c0c3f198a62655d
Patricia Sampaio <patysiq@yahoo.com.br>
2024-03-02 09:14:06 -0300

    Initial Commit

---
+++ oneLeftGame/oneLeftGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneLeftGameApp.swift
+//  oneLeftGame
+//
+//  Created by PATRICIA S SIQUEIRA on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct oneLeftGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
